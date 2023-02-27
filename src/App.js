70d00946fbf6ca2cc3b3a1b126a0a2425dd7e6d5
S0vers <sovers.pandey@outlook.com>
2023-02-27 12:20:59 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0)
+  function increment() {
+    setCount(count + 1)
+  }
+  function decrement() {
+    setCount(count - 1)
+  }
+  function reset() {
+    setCount(0)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='text-center grid h-screen place-items-center bg-cyan-200'>
+      <div className='content-center'>
+        <p className='text-4xl font-semibold text-gray-600'>Total Clicks</p>
+        <p className='text-7xl font-extrabold text-rose-600'>{count}</p>
+        <div className='py-5 grid lg:grid-cols-3 grid-cols-1 gap-4'>
+          <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded' type='button' onClick={increment}>Increment</button>
+          <button className='bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded' type='button' onClick={reset}>Reset</button>
+          <button className='bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded' type='button' onClick={decrement}>Decrement</button>
+        </div>
+      </div>
     </div>
   );
 }
